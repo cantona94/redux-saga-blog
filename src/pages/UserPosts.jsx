26ae46c811632fb95159866ed5d9ae50f67afd93
cantona94 <vlstan@mail.ru>
2023-06-06 12:19:38 +0500

    added postList in UserPosts page

--- src/pages/UserPosts.jsx
+++ src/pages/UserPosts.jsx
@@ -1,22 +1,40 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { LOAD_USER } from '../redux/actions/user';
-import { selectUser } from '../redux/selectors';
-import { UserCard } from '../components/index';
+import { LOAD_POSTS } from '../redux/actions/posts';
+import { selectUser, selectPosts } from '../redux/selectors';
+import { UserCard, PostList } from '../components/index';
 
 export const UserPosts = () => {
   const params = useParams();
   const userData = useSelector(selectUser);
+  const postsData = useSelector(selectPosts);
   const dispatch = useDispatch();
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     dispatch({ type: LOAD_USER, payload: { userId: params.userId } });
-  }, [])
+    dispatch({
+      type: LOAD_POSTS,
+      payload: {
+        userId: params.userId,
+        page: page,
+        limit: postsData.limit,
+      }
+    })
+  }, [page])
 
   return (
     <>
       <UserCard user={userData.users[0]} loading={userData.loading} />
+      <PostList
+          posts={postsData.posts}
+          loading={postsData.loading}
+          title={'User posts'}
+          page={page}
+          setPage={setPage}
+      />
     </>
   )
 }
